6ba7d2980719a1db14d63d842f1c9be38a239d9a
Augusta Carla Klug <augustaklug@gmail.com>
2024-09-10 23:58:03 -0300

    fixed CorsConfig.java

--- backend/src/main/java/com/klug/projectmanager/config/CorsConfig.java
+++ backend/src/main/java/com/klug/projectmanager/config/CorsConfig.java
@@ -1,22 +1,35 @@
 package com.klug.projectmanager.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.Arrays;
+
 @Configuration
 public class CorsConfig {
+
+    @Value("${cors.allowed-origins}")
+    private String[] allowedOrigins;
 
     @Bean
     public CorsFilter corsFilter() {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
+
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:3000"); // URL do frontend
+        config.setAllowedOrigins(Arrays.asList(allowedOrigins));
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
+
+        // Add these lines to handle preflight requests
+        config.addExposedHeader("Access-Control-Allow-Origin");
+        config.addExposedHeader("Access-Control-Allow-Methods");
+        config.addExposedHeader("Access-Control-Allow-Headers");
+
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
